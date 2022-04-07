9e79722b48148b141cf2808536358cacde9914a3
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-04-06 22:07:11 -0300

    :green_circle: Adiciona Principal.cs

--- ControleGastos/Program.cs
+++ ControleGastos/Program.cs
@@ -16,7 +16,7 @@ namespace ControleGastos
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Principal());
         }
     }
 }
